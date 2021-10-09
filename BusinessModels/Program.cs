bf4a189249d43436f80b645b50c30939ea58716a
ahnafakif <48433905+ahnaf-akif@users.noreply.github.com>
2021-10-09 10:52:35 +0600

    Add files via upload

---
+++ BusinessModels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessModels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
